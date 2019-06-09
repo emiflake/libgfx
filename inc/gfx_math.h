5bb07f80733ee1fe89c32b3176926b3f64ec92e5
Emily Martins <nmartins@student.codam.nl>
2019-06-09 15:17:52 +0200

    feat: linear interpolation

--- inc/gfx_math.h
+++ inc/gfx_math.h
@@ -6,7 +6,7 @@
 /*   By: nmartins <nmartins@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/30 14:16:17 by nmartins       #+#    #+#                */
-/*   Updated: 2019/05/02 20:28:29 by nmartins      ########   odam.nl         */
+/*   Updated: 2019/06/09 14:14:06 by nmartins      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # define RANGE_LERP(n,l,u,mi,ma) (((u)-(l))*((x)-(mi))/((ma)-(mi)))+(l)
 # define SIGN(n) (n > 0 ? 1 : -1)
 
-int	gfx_math_clamp(int value, int min, int max);
+int		gfx_math_clamp(int value, int min, int max);
+double	gfx_math_lerp(double a, double b, double f);
 
 #endif
